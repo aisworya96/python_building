2330fa478e669ec3f9f9b52840e44bbedca424b6
apple <aisworyapanda@gmail.com>
2025-03-03 22:50:06 +0530

    My first commit of the day4

--- Buggy Calculator/text.py
+++ Buggy Calculator/text.py
@@ -12,4 +12,10 @@ def divide(a, b):
         return "Error: Division by zero is not allowed"
     return a / 0
 
+def calculator():
+    print("Simple Calculator")
+    num1 = input("Enter first number: ")  # Bug: Should convert input to int or float
+    num2 = input("Enter second number: ")
+
+
 
