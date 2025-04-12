8bed84a89a12b8997951c9b697317bc3589be0cb
apple <aisworyapanda@gmail.com>
2025-04-12 20:15:47 +0530

    new statement added

--- Buggy Calculator/read_file.py
+++ Buggy Calculator/read_file.py
@@ -1,12 +1,22 @@
-name = "Aisworya"
-greeting = f"Hello, {name}"
-print(greeting)
+# name = "Aisworya"
+# greeting = f"Hello, {name}"
+# print(greeting)
+#
+# name = "Panda"
+# greeting = "Hello, {}"
+# with_name = greeting.format(name)
+#
+# name = input("Enter a name:")
+# print(name)
 
-name = "Panda"
-greeting = "Hello, {}"
-with_name = greeting.format(name)
+# first python app
 
-name = input("Enter a name:")
+user_age = input("Enter the age:")
+years = int(user_age)
+months = years * 12
+print(f"Yours age, {years}, is equal to {months}months.")
+
+
 
 
 
