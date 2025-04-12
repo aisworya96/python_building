3aefcab74d6cfe03b5b1b9361899d7405d45bdbf
apple <aisworyapanda@gmail.com>
2025-04-12 20:49:05 +0530

    modified1

--- Buggy Calculator/read_file.py
+++ Buggy Calculator/read_file.py
@@ -16,8 +16,9 @@ user_age = int(input("Enter the age:"))
 months = user_age * 12
 print(f"Yours age, {user_age}, is equal to {months}months.")
 
-list = ["apple", "orange", "banana"]
-
+list = ["apple", "orange", "banana"] #list has square brackates, can  modify a list, dont change the order
+tuple = ("apple", "orange", "banana") #you cannot modify a tuple, moon bracket, dont change the order
+set = {"apple", "orange", "banana"}# curly braces, add and remove elements from set, but cannot have duplicate element , order can chenge
 
 
 
